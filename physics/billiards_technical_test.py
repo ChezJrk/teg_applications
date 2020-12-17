094be11887947bfc00368527d0e861b8a8231b02
dromaiidae <dromaiidae@users.noreply.github.com>
2020-12-17 15:43:19 -0800

    Add physics examples init code

---
+++ physics/billiards_technical_test.py
@@ -0,0 +1,227 @@
+from typing import List, Tuple
+import time
+import collections
+import itertools
+import math
+import numpy as np
+import scipy as sp
+import scipy.optimize as spop
+import matplotlib.pyplot as plt
+
+from teg.lang.integrable_program import (
+    ITeg,
+    Const,
+    Var,
+    IfElse,
+    Teg,
+    Tup,
+    LetIn,
+    TegVar,
+    And,
+    Or,
+    Bool,
+)
+from teg.lang.integrable_program import false as TegFalse
+import teg.math.smooth as smooth
+from teg.derivs import FwdDeriv, RevDeriv
+from teg.eval import numpy_eval as evaluate_numpy
+from teg.passes import simplify
+from teg.passes import substitute
+from teg.ir import emit
+from tests.c_utils import runProgram, compileProgram
+from tap import Tap
+
+
+# logistic sigmoid
+def sigmoid(t: ITeg):
+    return 1/(1 + smooth.Exp(-1000 * t))
+
+
+def remap(t, t0, t1, x0, x1):
+    s = (t - t0) * (1 / (t1 - t0))
+    return x0 + s*(x1 - x0)
+
+
+# linear spline expression through xs; linear runtime
+def linspline(t: ITeg, ts: List[ITeg], xs: List[ITeg]):
+    # assert len(ts) == len(xs)
+    # assert len(ts) > 0
+    #
+    # f = xs[0]
+    # for pt, nt, px, nx in zip(ts, ts[1:], xs, xs[1:]):
+    #     m = (px - nx) / (pt - nt)
+    #     b = px - m * pt
+    #     ramp = m * t + b
+    #     f = IfElse(t < pt, f, ramp)
+    # f = IfElse(t < ts[-1], f, xs[-1])
+    # return f
+    assert len(ts) == len(xs)
+    assert len(ts) > 0
+
+    f = Const(0)
+    for idx, (t0, t1, x0, x1) in enumerate(zip(ts, ts[1:], xs, xs[1:])):
+        indicator = t < t1 if idx == 0 else t0 <= t if idx == (len(ts) - 2) else And(t0 <= t, t < t1)
+        lerp01 = remap(t, t0, t1, x0, x1)
+        f += IfElse(indicator, lerp01, Const(0))
+    return f
+
+
+# linear spline expression through xs; linear runtime
+def condition_linspline(t: ITeg, ts: List[ITeg], xs: List[ITeg], bool_func):
+    assert len(ts) == len(xs)
+    assert len(ts) > 0
+
+    f = TegFalse
+    for idx, (t0, t1, x0, x1) in enumerate(zip(ts, ts[1:], xs, xs[1:])):
+        indicator = t < t1 if idx == 0 else t0 <= t if idx == (len(ts) - 2) else And(t0 <= t, t < t1)
+        lerp01 = remap(t, t0, t1, x0, x1)
+        f = Or(f, And(indicator, bool_func(lerp01)))
+
+    return f
+
+
+# cubic hermite spline expression through xs; linear runtime
+def cubic_hermite_spline(t: ITeg, ts: List[ITeg], xs: List[ITeg], dxs:List[ITeg]):
+    assert len(ts) == len(xs)
+    assert len(ts) == len(dxs)
+    assert len(ts) > 0
+
+    f = xs[0]
+    for idx, (pt, nt, px, nx, pdx, ndx), in enumerate(zip(ts, ts[1:], xs, xs[1:], dxs, dxs[1:])):
+        u = (t - pt) / (nt - pt)  # == t normalized to [0, 1]
+        u2 = u * u
+        u3 = u * u2
+        ramp = (2 * u3 - 3 * u2 + 1) * px + (u3 - 2 * u2 + u) * pdx + (-2 * u3 + 3 * u2) * nx + (u3 - u2) * ndx
+        f = ramp if idx == 0 else IfElse(t < pt, f, ramp)
+    return f
+
+
+def catmull_spline(t: ITeg, ts: List[ITeg], xs: List[ITeg]):
+    assert len(ts) == len(xs)
+    assert len(ts) > 0
+
+    dxs = []
+    ts_ = [ts[0], *ts, ts[-1]]
+    xs_ = [xs[0], *xs, xs[-1]]
+    for idx in range(1, len(ts) + 1):
+        pt = ts_[idx - 1]
+        nt = ts_[idx + 1]
+        px = xs_[idx - 1]
+        nx = xs_[idx + 1]
+        dxs.append((nx - px) / (nt - pt))
+
+    return cubic_hermite_spline(t, ts, xs, dxs)
+
+
+def test_func(t, a, m):
+    args = Args()
+    t_ = TegVar('t_')
+
+    # a_ = a
+    # a = t
+    params = [a]
+
+    ts = [0, 1, 2]
+    xs = [0, a, 0]
+    x = linspline(t_, ts, xs)
+    x_cond = condition_linspline(t_, ts, xs, lambda expr: (1 < expr))
+    k = 10000
+    potential = k * IfElse(x_cond, smooth.Sqr(x - 1), 0)
+    # potential = k * IfElse(x_cond, 1, 0)
+
+    v = FwdDeriv(x, [(t_, 1), *[(_, 0) for _ in params]]).deriv_expr
+    kinetic = 1/2*m*v*v
+    lagrangian = kinetic - potential
+    action = Teg(0, 2, lagrangian, t_)
+    dSda = RevDeriv(action, Tup(Const(1)))
+    dels = dSda.variables
+    print(dels)
+
+    def bind_param(expr, vs):
+        for param, v in zip(params, vs):
+            expr.bind_variable(param, v)
+
+    def action_func(vs):
+        bind_param(action, vs)
+        return evaluate_numpy(-action, num_samples=args.num_samples, ignore_cache=True)
+
+    def d_action_func(vs):
+        bind_param(dSda, vs)
+        k = evaluate_numpy(dSda, num_samples=args.num_samples, ignore_cache=True)
+        return k[0]
+
+    init_guess = [2.0]
+    # init_guess = [1.03571428]
+    print(d_action_func(init_guess))
+    print(action_func(init_guess))
+    # res = spop.root_scalar(d_action_func, bracket=[0.1, 2.0], method='bisect')
+    res = spop.root(d_action_func, x0=init_guess, method='hybr', tol=1e-8)
+    # res = spop.minimize(action_func, init_guess, method='Nelder-Mead', tol=1e-12)
+    # res = spop.minimize(action_func, init_guess, method='CG', tol=1e-6, jac=loss_grad)
+
+    print(res.x)
+    print(d_action_func(res.x))
+    x.bind_variable(a, res.x)
+    return substitute.substitute(x, t_, t)
+    # return action
+    # return dSda
+
+
+class Args(Tap):
+    pixel_width: int = 30
+    pixel_height: int = 30
+    num_samples: int = 30
+    t_samples: int = 2000
+
+
+def main():
+    args = Args()
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+
+    a0 = 1.5
+    m0 = 1
+    t = Var('t')
+    a = Var('a', a0)
+    m = Var('m', m0)
+    param_vars = [
+        a,
+        m,
+    ]
+    differential_vals = collections.defaultdict(int, {
+        'a': 1,
+        'm': 0,
+    })
+
+    teg_func = test_func
+
+    def sample_expr(expr, ts):
+        for t_ in ts:
+            print(t_)
+            expr.bind_variable(t, t_)
+            f = evaluate_numpy(expr, num_samples=args.num_samples, ignore_cache=True)
+            # print(t_, f)
+            yield f
+
+    t_samples = 40
+    def map_sample_func(f, ts):
+        return Tup(*[
+            f(Const(t), a, m)
+            for t in ts
+        ])
+
+    ts = [(2*i)/args.t_samples for i in range(args.t_samples + 1)]
+    fexpr = teg_func(t, a, m)
+    xs = list(sample_expr(fexpr, ts))
+
+    axes[0].plot(ts, xs)
+    # axes[1].plot(ts, dxs)
+
+
+
+
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ physics/gaussian_rasterize.py
@@ -0,0 +1,144 @@
+from typing import List, Tuple
+import time
+import collections
+import numpy as np
+import matplotlib.pyplot as plt
+
+from teg.lang.integrable_program import (
+    ITeg,
+    Const,
+    Var,
+    IfElse,
+    Teg,
+    Tup,
+    LetIn,
+    TegVar,
+)
+import teg.math.smooth as smooth
+from teg.derivs import FwdDeriv
+from teg.eval import numpy_eval as evaluate_numpy
+from teg.passes.simplify import simplify
+from teg.ir import emit
+from tests.c_utils import runProgram, compileProgram
+from tap import Tap
+
+
+class Args(Tap):
+    pixel_width: int = 30
+    pixel_height: int = 30
+    num_samples: int = 30
+
+
+def evaluate_c(expr: ITeg, num_samples=5000, ignore_cache=False, silent=False):
+    pcount_before = time.perf_counter()
+    c_code = emit(expr, target='C', num_samples=num_samples)
+    pcount_after = time.perf_counter()
+    if not silent:
+        print(f'Teg-to-C emit time: {pcount_after - pcount_before:.3f}s')
+
+    pcount_before = time.perf_counter()
+    binary = compileProgram(c_code)
+    pcount_after = time.perf_counter()
+    if not silent:
+        print(f'C compile time:     {pcount_after - pcount_before:.3f}s')
+
+    pcount_before = time.perf_counter()
+    value = runProgram(binary)
+    pcount_after = time.perf_counter()
+    if not silent:
+        print(f'C exec time:        {pcount_after - pcount_before:.3f}s')
+
+    return value
+
+
+def evaluate(*args, **kwargs):
+    fast_eval = kwargs.pop('fast_eval', False)
+    if fast_eval:
+        if not kwargs.get('silent', True):
+            print('Evaluating in fast-mode: C')
+        return evaluate_c(*args, **kwargs)
+    else:
+        if not kwargs.get('silent', True):
+            print('Evaluating using numpy/python')
+        # kwargs['ignore_cache'] = True
+        return evaluate_numpy(*args, **kwargs)
+
+
+def rasterize_triangles():
+    x, y = TegVar('x'), TegVar('y')
+    theta = Var('theta', 1)
+    A = Var('a', 1)
+    mux = Var('mx', 0.5)
+    muy = Var('mx', 0.5)
+    sigmax = Var('sx', 0.15)
+    sigmay = Var('sy', 0.15)
+
+    param_vars = [
+        A,
+        mux,
+        muy,
+        sigmax,
+        sigmay,
+    ]
+
+    def right_triangle(x0, y0):
+        """ ◥ with upper right corner at (x0, y0) """
+        return (y < y0) & (x < x0 + theta) & (x - x0 + y - y0 + 0.75 + theta > 0)
+
+    inside_front_cond = right_triangle(0.7, 0.7)
+
+    # body = IfElse(inside_front_cond, 1, 0)
+    # body = IfElse((y < .7 + theta) & (x < .7 + theta) & (.65 + theta < x + y), 1, 0)
+
+    def gaussian(x_, y_, a, mx, my, sx, sy):
+        # return a * smooth.Exp(-0.5 * smooth.Sqr((y_ - my) / sy)) / (sy * 2.5066) \
+        #          * smooth.Exp(-0.5 * smooth.Sqr((x_ - mx) / sx)) / (sx * 2.5066)
+        return a * smooth.Exp(-0.5 * (smooth.Sqr((x_ - mx) / sx) + smooth.Sqr((y_ - my) / sy))) / (sx * sy * 2 * np.pi)
+
+
+    body = gaussian(x, y, A, mux, muy, sigmax, sigmay)
+    # body = smooth.Sqr(x - mux)
+    # body = (x - mux) * (x - mux)
+
+    w, h = args.pixel_width, args.pixel_height
+    inv_area = w * h
+    pixel_expr = Tup(*[
+        Teg(i / w, (i + 1) / w, Teg(j / h, (j + 1) / h, body, x), y) * inv_area
+        for i in range(w)
+        for j in range(h)
+    ])
+    return pixel_expr, param_vars
+
+
+if __name__ == '__main__':
+    args = Args().parse_args()
+
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    pixel_expr, param_vars = rasterize_triangles()
+
+    pcount_before = time.perf_counter()
+
+    w, h = args.pixel_width, args.pixel_height
+    res = evaluate(pixel_expr, num_samples=args.num_samples)
+    pixel_grid = np.array(res).reshape((w, h))
+    # axes[0].imshow(pixel_grid[::-1, :], vmin=-1/(w * h), vmax=1/(w * h))
+    axes[0].imshow(pixel_grid, origin='lower', extent=(0, 1, 0, 1))
+
+    import math
+    differential_vals = collections.defaultdict(int, {
+        'a': 0,
+        'mx': 0,
+        'my': 0,
+        'sx': 1,
+        'sy': 0,
+    })
+    deriv_expr = FwdDeriv(pixel_expr, [(theta, differential_vals[theta.name]) for theta in param_vars])
+
+    res = evaluate(deriv_expr, num_samples=args.num_samples)
+    pixel_grid = np.array(res).reshape((w, h))
+    # axes[1].imshow(pixel_grid[::-1, :], vmin=-0.05, vmax=0.05)
+    axes[1].imshow(pixel_grid, origin='lower', extent=(0, 1, 0, 1))
+    pcount_after = time.perf_counter()
+    print(f'total:\t{pcount_after - pcount_before}')
+    plt.show()
+
