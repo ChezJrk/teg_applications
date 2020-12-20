3880c916900bd08e7ece6377c98ae84f430246a6
dromaiidae <dromaiidae@users.noreply.github.com>
2020-12-19 23:48:59 -0800

    Remove hack, add submodule to requirements

--- physics/gaussian_rasterize.py
+++ physics/gaussian_rasterize.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append("../Teg")  # TODO: hacky; actually engineer a proper setup.py
 from typing import List, Tuple
 import time
 import collections
--- probabilistic_programming/distinguishing_samples.py
+++ probabilistic_programming/distinguishing_samples.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append("../Teg")  # TODO: hacky; actually engineer a proper setup.py
 import math
 import matplotlib.pyplot as plt
 
