990d95dfa685bd4b1f5d32f7665b465883f5c816
Jesse Michel <jmmichel@mit.edu>
2021-01-02 20:42:19 -0500

    Add threshold to optimize

--- physics/springs.py
+++ physics/springs.py
@@ -9,53 +9,64 @@ from teg.lang.integrable_program import (
     Tup,
     LetIn,
     TegVar,
-    FwdDeriv,
+    RevDeriv,
 )
 from teg.math.smooth import Sqrt
 from teg.passes.evaluate import evaluate
 
 
-def stress(strain: ITeg) -> ITeg:
-    yield_strength = 10
+def stress(strain: ITeg, yield_strength: Var) -> ITeg:
     stress = IfElse(strain < yield_strength, 2 * strain, strain**2 / 5)
     return stress
 
 
-def solve_for_time_given_position(position: Const) -> ITeg:
+def solve_for_time_given_position(position: Const, yield_strength: Var) -> ITeg:
     x = TegVar('x')
     x_hat = TegVar('x_hat')
     x_hat_hat = Const(name='x_hat_hat', value=position.value)
     c0 = Const(10)
     c1 = Const(10)
-    expr = 1 / Sqrt(2 * Teg(0, x_hat, -stress(x), x) + c0)
+    expr = 1 / Sqrt(2 * Teg(0, x_hat, -stress(x, yield_strength), x) + c0)
     ode_solution_wrt_time = Teg(0, x_hat_hat, expr, x_hat) + c1
     return ode_solution_wrt_time
 
 
-def optimize_scale():
+def optimize():
+    """Optimizing both yield strength and scale for springiness. """
+    # Parameters to optimize
     scale = Var('scale', 1)
+    yield_strength = Var('yield_strength', 10)
     ground_height = 100
+
+    # Set up the loss
     position = Const(1)
     penalty_value = Const(10)
-    penalty = IfElse(x < ground_height, 0, penalty_value)
-    loss = solve_for_time_given_position(position) + penalty
+    penalty = IfElse(x >= ground_height, penalty_value, 0)
+    loss = solve_for_time_given_position(position, yield_strength) + penalty
 
     # Optimize by gradient descent
-    num_samples = 50
+    num_samples = 10
     loss_values = [evaluate(loss, num_samples=num_samples, ignore_cache=True)]
     scale_values = [scale.value]
+    yield_strength_values = [yield_strength.value]
     for i in range(25):
-        dscale = evaluate(FwdDeriv(loss, (scale, 1)), num_samples=num_samples, ignore_cache=True)
+        dscale, dyield_strength = evaluate(RevDeriv(loss, Tup(Const(1))), num_samples=num_samples, ignore_cache=True)
+
+        # Take gradient step
         step_size = 0.5
+        yield_strength.value -= step_size * dyield_strength
         scale.value -= step_size * dscale
+
+        # Save data for plotting
         loss_values.append(evaluate(loss, num_samples=num_samples, ignore_cache=True))
         scale_values.append(scale.value)
+        yield_strength_values.append(yield_strength.value)
 
-    return loss_values, scale_values
+    return loss_values, scale_values, yield_strength_values
 
 
 if __name__ == "__main__":
-    loss_values, scale_values = optimize_scale()
+    loss_values, scale_values, yield_strength_values = optimize()
 
     plt.plot(loss_values)
     plt.ylabel('Value of Loss')
@@ -64,5 +75,10 @@ if __name__ == "__main__":
 
     plt.plot(scale_values)
     plt.ylabel('Scale of Stress')
+    plt.xlabel('Iteration of Gradient Descent')
+    plt.show()
+
+    plt.plot(yield_strength_values)
+    plt.ylabel('Yield Strength Threshold')
     plt.xlabel('Iteration of Gradient Descent')
     plt.show()
