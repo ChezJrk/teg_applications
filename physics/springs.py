2df1edd9a08783b755740d353e04fb69c6cb616c
Jesse Michel <jmmichel@mit.edu>
2021-01-04 01:23:12 -0500

    Still broken, but with much progress

--- physics/springs.py
+++ physics/springs.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from tqdm import trange
 
 from teg import (
     ITeg,
@@ -11,62 +12,109 @@ from teg import (
     TegVar,
 )
 from teg.derivs import RevDeriv
-from smooth import InvertSqrt
+from smooth import InvertSqrt, IsNotNan
+from teg.math.smooth import Invert, Sqrt
 from teg.eval.numpy_eval import evaluate
+from teg.passes.substitute import substitute
+from teg.passes.simplify import simplify
 
 
-def stress(strain: ITeg, yield_strength: Var) -> ITeg:
-    # TODO: parameterize in terms of yield_strength
-    stress = IfElse(strain < yield_strength, 2 * strain, strain**2 / 5)
-    return stress
+def stress(strain: ITeg, yield_strength: Var, scale: Var) -> ITeg:
+    """Stress curve given the strain as in Young's Modulus. """
+    a, h = yield_strength, 2.2 * yield_strength
+
+    # A line with slope 2 from 0 to a
+    before_yield = 2 * strain
+
+    # A parabola from a to 2a and symmetric about 3a / 2 that is less steep than the line
+    after_yield = 4 * ((2 * a - h) / (a**2)) * (strain - 3 * a / 2)**2 + h
+
+    # Discontinuous stress as described by Young's Modulus
+    stress = IfElse(strain < yield_strength, before_yield, after_yield)
+
+    return scale * stress
 
 
-def solve_for_time_given_position(position: Const, yield_strength: Var, scale: Var) -> ITeg:
+def solve_for_time_given_position(position: Const, yield_strength: Var, scale: Var):
+    """The time at which the bungee is at a given position (height) assuming it begins
+    at rest position (0,0) and velocity. """
     x = TegVar('x')
     x_hat = TegVar('x_hat')
     ground_height = 100
-    penalty_value = 2
+    penalty = 100
     gravity = 10
 
-    expr = InvertSqrt(2 * Teg(0, x_hat, gravity - scale * stress(x, yield_strength), x))
-    penalty = IfElse(x >= ground_height, penalty_value, 0)
-    ode_solution_wrt_time = Teg(0, position, expr + penalty, x_hat)
-    return ode_solution_wrt_time
+    # Solution to the second-order linear ODE
+    inner_integrand = gravity - stress(x, yield_strength, scale)
+    velocity = 2 * Teg(0, x_hat, inner_integrand, x)
+    expr = InvertSqrt(velocity)
+    ode_solution_wrt_time = Teg(0, position, expr, x_hat)
+
+    # TODO: there's a nan problem here. IfElse( > 0, Teg(0, ground_height, inner_integrand, x), 0)
+    activate_penalty = IsNotNan(Invert(Sqrt(Teg(0, ground_height, inner_integrand, x))))
+
+    return ode_solution_wrt_time + penalty * activate_penalty, activate_penalty
 
 
-def optimize():
+def optimize(scale: Var, yield_strength: Var):
     """Optimizing both yield strength and scale for springiness. """
-    # Parameters to optimize
-    scale = Var('scale', 5)
-    yield_strength = Var('yield_strength', 3)
-
     # Set up the loss
-    position = Const(5)
-    loss = solve_for_time_given_position(position, yield_strength, scale)
+    position = Const(10)
+    loss, activate_penalty = solve_for_time_given_position(position, yield_strength, scale)
 
     # Optimize by gradient descent
-    num_samples = 10
-    loss_values = [evaluate(loss, num_samples=num_samples, ignore_cache=True)]
-    scale_values = [scale.value]
-    yield_strength_values = [yield_strength.value]
-    for i in range(100):
-        dscale, dyield_strength = evaluate(RevDeriv(loss, Tup(Const(1))), num_samples=num_samples, ignore_cache=True)
-
-        # Take gradient step
-        step_size = 0.5
-        # yield_strength.value -= step_size * dyield_strength
-        scale.value -= step_size * dscale
-
+    num_samples = 30
+    loss_values = []
+    scale_values = []
+    yield_strength_values = []
+    loss = simplify(loss)
+    for i in trange(40):
         # Save data for plotting
         loss_values.append(evaluate(loss, num_samples=num_samples, ignore_cache=True))
         scale_values.append(scale.value)
         yield_strength_values.append(yield_strength.value)
 
+        # Derivative computation
+        deriv = RevDeriv(loss, Tup(Const(1))).deriv_expr
+        deriv = substitute(deriv, activate_penalty.blew_up, Const(0) if activate_penalty.errored else Const(1))
+        dscale, dyield_strength = evaluate(simplify(deriv), num_samples=num_samples, ignore_cache=True)
+
+        # Take gradient step
+        step_size = 0.1
+        print(dyield_strength, dscale)
+        yield_strength.value -= step_size * dyield_strength
+        scale.value -= step_size * dscale
+
+    loss_values.append(evaluate(loss, num_samples=num_samples, ignore_cache=True))
+    scale_values.append(scale.value)
+    yield_strength_values.append(yield_strength.value)
+
     return loss_values, scale_values, yield_strength_values
 
 
 if __name__ == "__main__":
-    loss_values, scale_values, yield_strength_values = optimize()
+    # Parameters to optimize
+    scale_init = 3
+    yield_strength_init = 5
+    scale = Var('scale', scale_init)
+    yield_strength = Var('yield_strength', yield_strength_init)
+
+    loss_values, scale_values, yield_strength_values = optimize(scale, yield_strength)
+
+    import numpy as np
+    strains = np.arange(0, yield_strength_init*2, step=0.1)
+    stresses_before = [evaluate(stress(Const(strain), yield_strength_init, scale_init))
+                       for strain in strains]
+    stresses_after = [evaluate(stress(Const(strain), yield_strength_values[-1], scale_values[-1]))
+                      for strain in strains]
+
+    print(scale_values, yield_strength_values)
+    plt.plot(strains, stresses_before, label='Before')
+    plt.plot(strains, stresses_after, label='After')
+    plt.ylabel('Stress')
+    plt.xlabel('Strain')
+    plt.legend()
+    plt.show()
 
     plt.plot(loss_values)
     plt.ylabel('Value of Loss')
